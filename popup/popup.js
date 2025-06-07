7cd81aa650c21b50de217d1725bd82e93d1f03d6
meisullum <meisullum@gmail.com>
2025-06-07 16:09:48 -0500

    Functioning tasks can be added and removed properly.. Also uses chrome storage to save

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,37 @@
+let tasks = []
+
 const addTasks = document.getElementById("add-task-btn")
 addTasks.addEventListener("click", ()=> addTask())
 
-function addTask () {
+chrome.storage.sync.get(["tasks"], (res) => {
+    tasks = res.tasks ? res.tasks : []
+    renderTasks()
+})
+
+function saveTasks() {
+    chrome.storage.sync.set({
+        tasks,
+    })
+}
+
+function renderTask(tasksLength) {
     const taskRow=document.createElement("div")
+
     const text = document.createElement("input")
     text.type="text"
     text.placeholder="Enter a task..."
+    text.value=tasks[tasksLength]
+    text.addEventListener("change", () => {
+        tasks[tasksLength] = text.value
+        saveTasks()
+    })
 
     const deleteBtn = document.createElement("input")
     deleteBtn.type="button"
     deleteBtn.value="X"
+    deleteBtn.addEventListener("click", () => {
+        deleteTask(tasksLength)
+    })
 
     taskRow.appendChild(text)
     taskRow.appendChild(deleteBtn)
@@ -19,6 +41,23 @@ function addTask () {
 }
 
 
-function removeTask() {
-    
+function addTask () {
+    const tasksLength = tasks.length
+    tasks.push("")
+    renderTask(tasksLength)
+    saveTasks()
+}
+
+function deleteTask(tasksLength) {
+    tasks.splice(tasksLength, 1)
+    renderTasks()
+    saveTasks()
+}
+
+function renderTasks() {
+    const taskContainer = document.getElementById("task-container")
+    taskContainer.textContent = ""
+    tasks.forEach((taskTest, taskNum) => {
+        renderTask(taskNum)
+    })
 }
